d749a6c6a852cbcc1da58313adc0c2a8ea1e6528
Han <quaint3@gmail.com>
2020-07-21 17:11:40 +0900

    [#11] Feat : 기본 Error Handle 설정
    - Entity NotFound에 대한 에러 처리
    - 추후, 인증등의 에러 핸들링 추가 예정

---
+++ src/main/java/com/codesquad/rare/error/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.codesquad.rare.error;
+
+import static com.codesquad.rare.common.api.ApiResult.ERROR;
+
+import com.codesquad.rare.common.api.ApiResult;
+import com.codesquad.rare.error.exeception.NotFoundException;
+import com.codesquad.rare.error.exeception.ServiceRuntimeException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  private ResponseEntity<ApiResult> newResponse(Exception exception, HttpStatus status) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    return new ResponseEntity<>(ERROR(exception, status), headers, status);
+  }
+
+  private ResponseEntity<ApiResult> newResponseWithString(String message, HttpStatus status) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    return new ResponseEntity<>(ERROR(message, status), headers, status);
+  }
+
+  // 예상 하지 못했던 오류
+  @ExceptionHandler({Exception.class, RuntimeException.class})
+  public ResponseEntity<?> handleException(Exception e) {
+    log.error("Unexpected exception occurred: {}", e.getMessage(), e);
+    return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(ServiceRuntimeException.class)
+  public ResponseEntity<?> handleServiceRuntimeException(ServiceRuntimeException e) {
+    if (e instanceof NotFoundException) {
+      log.error("NotFound exception occurred: {}", e.getMessage(), e);
+      return newResponse(e, HttpStatus.NOT_FOUND);
+    }
+
+    log.error("Unexpected service exception occurred: {}", e.getMessage(), e);
+    return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+}
