f04f68628dc6034384ccddcdd15bed3722b1ddfc
Han <quaint3@gmail.com>
2020-08-07 16:49:56 +0900

    [#73] Feat : 단계별 echo 추가
    - 단계별로 진행되고 있는 상황을 파악하고자 추가함

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOSITORY=/home/ubuntu/rare
 cd $REPOSITORY
 
 APP_NAME=rare
+
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+
+echo "> JAR_NAME 확인 : $JAR_NAME"
+
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+echo "> JAR_PATH 확인: $JAR_PATH"
+
 CURRENT_PID=$(pgrep -f $APP_NAME)
+
+echo "> 현재 구동중인 애플리케이션 PID 확인 : $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]
 then
@@ -20,3 +28,4 @@ fi
 
 echo "> $JAR_PATH 배포"
 nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> 배포 완료"
