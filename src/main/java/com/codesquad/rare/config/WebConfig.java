330be898ca888f96c5507cb7963c43a7468cfa6a
Han <quaint3@gmail.com>
2020-08-07 19:44:01 +0900

    [#79] Feat : EnableWebMvc 어노테이션 추가

--- src/main/java/com/codesquad/rare/config/WebConfig.java
+++ src/main/java/com/codesquad/rare/config/WebConfig.java
@@ -9,9 +9,11 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   private static final String DATE_FORMAT = "yyyy-MM-dd";
