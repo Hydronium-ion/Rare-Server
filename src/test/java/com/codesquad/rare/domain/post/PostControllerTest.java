2f042f90e2154fc4e525a45cb970b56b5288d2c6
Hamill210 <han95210@naver.com>
2020-08-07 12:23:03 +0900

    [#69] Modify : 코드 리뷰에 관한 사항 수정 및 삭제

--- src/test/java/com/codesquad/rare/domain/post/PostControllerTest.java
+++ src/test/java/com/codesquad/rare/domain/post/PostControllerTest.java
@@ -30,7 +30,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
