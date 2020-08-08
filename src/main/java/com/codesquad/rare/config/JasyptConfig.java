1b469909fb28e346608d8ef08369e3965c5e74fd
Hamill210 <han95210@naver.com>
2020-08-09 02:57:44 +0900

    [#73] Mofidy : Bean 을 다른 걸로 바꿔 끼워줌

--- src/main/java/com/codesquad/rare/config/JasyptConfig.java
+++ src/main/java/com/codesquad/rare/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-  @Bean("encryptorBean")
+  @Bean("jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
