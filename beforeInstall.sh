04590dc980e752dd1ee8e0f951123f30bc9b7e46
Hamill210 <han95210@naver.com>
2020-08-09 01:46:36 +0900

    [#73] Feat : 배포 스크립트와 appspec 에 beforeInstall 추가

---
+++ beforeInstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [-d /home/ubuntu/rare ]; then
+  rm -rf /home/ubuntu/rare
+fi
+mkdir -vp /home/ubuntu/rare
--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH & 1> /home/ubuntu/nohup.out
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
