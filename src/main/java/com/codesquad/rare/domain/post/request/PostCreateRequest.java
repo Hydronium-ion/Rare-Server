9c6adafc214e2e638b8a02cb32ae956eaa7781a5
Han <quaint3@gmail.com>
2020-08-10 20:00:40 +0900

    [#82] Refactor : PostCreateRequest
    - final 추가
    - Builder 생성자에 추가

--- src/main/java/com/codesquad/rare/domain/post/request/PostCreateRequest.java
+++ src/main/java/com/codesquad/rare/domain/post/request/PostCreateRequest.java
@@ -2,33 +2,39 @@ package com.codesquad.rare.domain.post.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PostCreateRequest {
 
   @NotBlank(message = "제목을 반드시 입력해주세요")
-  private String title;
+  private final String title;
 
   @NotBlank(message = "부제목을 반드시 입력해주세요")
-  private String subTitle;
+  private final String subTitle;
 
-  private String content;
+  private final String content;
 
-  private String thumbnail;
+  private final String thumbnail;
 
   @NotNull(message = "작성자의 ID는 반드시 입력해주세요")
-  private Long authorId;
+  private final Long authorId;
 
-  private String tags;
+  private final String tags;
 
   @NotNull(message = "공개 여부를 반드시 입력해주세요")
-  private Boolean isPublic;
+  private final Boolean isPublic;
+
+  @Builder
+  private PostCreateRequest(String title, String subTitle, String content, String thumbnail,
+      Long authorId, String tags, Boolean isPublic) {
+    this.title = title;
+    this.subTitle = subTitle;
+    this.content = content;
+    this.thumbnail = thumbnail;
+    this.authorId = authorId;
+    this.tags = tags;
+    this.isPublic = isPublic;
+  }
 }
