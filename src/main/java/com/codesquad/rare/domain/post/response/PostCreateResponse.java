caabc286302c42dadfe590c90f83a785b180ea35
Han <quaint3@gmail.com>
2020-08-11 17:00:43 +0900

    [#82] Refactor : PostCreateResponse lombok 사용
    - 명시된 생성자를 제거하고 롬복에서 지원하는 어노테이션 사용

--- src/main/java/com/codesquad/rare/domain/post/response/PostCreateResponse.java
+++ src/main/java/com/codesquad/rare/domain/post/response/PostCreateResponse.java
@@ -1,18 +1,16 @@
 package com.codesquad.rare.domain.post.response;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @ToString
 public class PostCreateResponse {
 
   private Long postId;
-
-  public PostCreateResponse(Long postId) {
-    this.postId = postId;
-  }
 }
