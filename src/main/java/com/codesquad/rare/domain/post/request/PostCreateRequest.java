7c39a92fd3df351757611b2b71fc90565e41de9e
Han <quaint3@gmail.com>
2020-08-13 14:30:38 +0900

    [#82] Refactor : Post DTO, 생성자 매개변수 수정

--- src/main/java/com/codesquad/rare/domain/post/request/PostCreateRequest.java
+++ src/main/java/com/codesquad/rare/domain/post/request/PostCreateRequest.java
@@ -2,33 +2,37 @@ package com.codesquad.rare.domain.post.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostCreateRequest {
 
   @NotBlank(message = "제목을 반드시 입력해주세요")
-  private final String title;
+  private String title;
 
   @NotBlank(message = "부제목을 반드시 입력해주세요")
-  private final String subTitle;
+  private String subTitle;
 
-  private final String content;
+  private String content;
 
-  private final String thumbnail;
+  private String thumbnail;
 
   @NotNull(message = "작성자의 ID는 반드시 입력해주세요")
-  private final Long authorId;
+  private Long authorId;
 
-  private final String tags;
+  private String tags;
 
   @NotNull(message = "공개 여부를 반드시 입력해주세요")
-  private final Boolean isPublic;
+  private Boolean isPublic;
 
   @Builder
-  private PostCreateRequest(String title, String subTitle, String content, String thumbnail,
-      Long authorId, String tags, Boolean isPublic) {
+  private PostCreateRequest(final String title, final String subTitle,
+      final String content, final String thumbnail,
+      final Long authorId, final String tags, final Boolean isPublic) {
     this.title = title;
     this.subTitle = subTitle;
     this.content = content;
